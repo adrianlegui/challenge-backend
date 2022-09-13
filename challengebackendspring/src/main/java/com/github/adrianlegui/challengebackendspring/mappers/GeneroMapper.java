8e83ee155274f95f8c95195483ffb8e301c02d87
Adrian Leguizamon <adrian.luis.leguizamon@gmail.com>
2022-09-13 15:03:03 -0300

    feat(mappers): agregado métodos de mapeo a generoMapper

--- challengebackendspring/src/main/java/com/github/adrianlegui/challengebackendspring/mappers/GeneroMapper.java
+++ challengebackendspring/src/main/java/com/github/adrianlegui/challengebackendspring/mappers/GeneroMapper.java
@@ -1,9 +1,25 @@
 package com.github.adrianlegui.challengebackendspring.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import com.github.adrianlegui.challengebackendspring.dto.GeneroDTO;
+import com.github.adrianlegui.challengebackendspring.dto.GeneroDTOGET;
+import com.github.adrianlegui.challengebackendspring.dto.GeneroDTOPATCH;
+import com.github.adrianlegui.challengebackendspring.dto.GeneroDTOPOST;
+import com.github.adrianlegui.challengebackendspring.entities.GeneroEntity;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface GeneroMapper {
 
+	GeneroDTOPATCH dtoPostToDtoPath(GeneroDTOPOST generoDTOPOST);
+
+	GeneroEntity dtoPathtoEntity(GeneroDTOPATCH generoDTOPATCH);
+
+	GeneroDTO entityToDTO(GeneroEntity generoEntity);
+
+	GeneroDTOGET entityToDtoGet(GeneroEntity generoEntity);
+
+	GeneroEntity dtoPathtoEntity(GeneroDTOPATCH generoDTOPATCH, @MappingTarget GeneroEntity generoEntity);
 }
