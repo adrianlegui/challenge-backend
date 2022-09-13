e334e246f594bc7b523e0e04871c30447d043cbd
Adrian Leguizamon <adrian.luis.leguizamon@gmail.com>
2022-09-13 15:08:52 -0300

    feat(entities): agregado métodos add y remove a generoEntity
    Para mantener las asociaciones sincronizadas entre las entities.

--- challengebackendspring/src/main/java/com/github/adrianlegui/challengebackendspring/entities/GeneroEntity.java
+++ challengebackendspring/src/main/java/com/github/adrianlegui/challengebackendspring/entities/GeneroEntity.java
@@ -2,6 +2,7 @@ package com.github.adrianlegui.challengebackendspring.entities;
 
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.Entity;
@@ -33,6 +34,33 @@ public class GeneroEntity {
 	@ManyToMany(mappedBy = "generosDeLaSerie")
 	private Set<SerieEntity> series  = new HashSet<>();
 	
+	
+	public void addPelicula(PeliculaEntity... entities) {
+		for(PeliculaEntity pelicula : entities) {
+			this.peliculas.add(pelicula);
+			pelicula.getGenerosDeLaPelicula().add(this);
+		}
+	}
+	
+	
+	public void removePelicula(Long... peliculaIds) {
+		for(Long peliculaId : peliculaIds) {
+			PeliculaEntity pelicula = 
+					this.peliculas.stream()
+					.filter(
+							p -> Objects.equals(p.getId(), peliculaId)
+							)
+					.findFirst()
+					.orElse(null);
+			
+			if(pelicula != null) {
+				this.peliculas.remove(pelicula);
+				pelicula.getGenerosDeLaPelicula().remove(this);
+			}
+		}
+	}
+	
+	
 	public void removeAllPelicula() {
 		Iterator<PeliculaEntity> peliculaIterator = peliculas.iterator();
 		
@@ -42,6 +70,33 @@ public class GeneroEntity {
 			peliculaIterator.remove();
 		}
 	}
+	
+	
+	public void addSerie(SerieEntity... entities) {
+		for(SerieEntity serie : entities) {
+			this.series.add(serie);
+			serie.getGenerosDeLaSerie().add(this);
+		}
+	}
+	
+	
+	public void removeSerie(Long... generoIds) {
+		for(Long generoId : generoIds) {
+			SerieEntity serie = 
+					series.stream()
+					.filter(
+							s -> Objects.equals(s.getId(), generoId)
+							)
+					.findFirst()
+					.orElse(null);
+			
+			if(serie != null) {
+				series.remove(serie);
+				serie.getGenerosDeLaSerie().remove(this);
+			}
+		}
+	}
+	
 	
 	public void removeAllSerie() {
 		Iterator<SerieEntity> serieIterator = series.iterator();
