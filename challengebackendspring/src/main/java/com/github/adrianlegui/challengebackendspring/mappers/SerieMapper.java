1264cdca4b11d7821735c0a5a2bc2530c9aa20b7
Adrian Leguizamon <adrian.luis.leguizamon@gmail.com>
2022-09-14 17:35:41 -0300

    feat(mappers): nuevos métodos de mapeo a serieMapper

--- challengebackendspring/src/main/java/com/github/adrianlegui/challengebackendspring/mappers/SerieMapper.java
+++ challengebackendspring/src/main/java/com/github/adrianlegui/challengebackendspring/mappers/SerieMapper.java
@@ -1,9 +1,30 @@
 package com.github.adrianlegui.challengebackendspring.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+import com.github.adrianlegui.challengebackendspring.dto.SerieDTO;
+import com.github.adrianlegui.challengebackendspring.dto.SerieDTOGET;
+import com.github.adrianlegui.challengebackendspring.dto.SerieDTOPATCH;
+import com.github.adrianlegui.challengebackendspring.dto.SerieDTOPOST;
+import com.github.adrianlegui.challengebackendspring.entities.SerieEntity;
+
+@Mapper(
+	componentModel = "spring",
+	nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface SerieMapper {
+
+	SerieDTOPATCH dtoPostToDtoPatch(SerieDTOPOST serieDTOPOST);
+
+	SerieEntity dtoPatchToEntity(SerieDTOPATCH serieDTOPATCH);
+
+	SerieDTO entityToDTO(SerieEntity serieEntity);
+
+	SerieDTOGET entityToDtoGet(SerieEntity serieEntity);
+
+	SerieEntity dtoPatchToEntity(
+		SerieDTOPATCH serieDTOPATCH,
+		@MappingTarget SerieEntity serieEntity);
 
 }
